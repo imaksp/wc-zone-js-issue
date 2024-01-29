4a258887e810a949efb10f0c934606516a817ea0
Akash <2557058+aksdevac@users.noreply.github.com>
2024-01-29 18:28:38 +0530

    remove debugger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ export class AppComponent {
   title = 'wc-zone-js-issue';
 
   async connectClick() {
-    debugger;
     try {
       const provider = await EthereumProvider.init({
         projectId: '6e56925d35b71a3dec5f50a11923f356', // REQUIRED your projectId
